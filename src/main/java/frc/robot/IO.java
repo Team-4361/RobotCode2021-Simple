aebcacdf8e3b9505c155ea2a7c3564eb7d6c72ff
colin robertson <wobblyyyy@gmail.com>
2021-02-27 21:36:54 -0500

    continue working on control schemes

---
+++ src/main/java/frc/robot/IO.java
@@ -0,0 +1,149 @@
+package frc.robot;
+
+import frc.robot.controls.Colin;
+import frc.robot.controls.ControlScheme;
+
+/**
+ * Wrapper class for control schemes.
+ *
+ * <p>
+ * The way controls were originally set up in this codebase was great, if
+ * and only if you only ever wanted to use a singular control scheme. Which,
+ * in this case, we don't. Rather, we'd like to use however many different
+ * control schemes we would like to use.
+ * </p>
+ *
+ * <p>
+ * It would be a tremendous waste of time to focus exclusively on a single
+ * control scheme. The best way I could describe it would be "absolutely
+ * pointless" - not allowing for the development of multiple control schemes
+ * will greatly hinder the ability to switch out controls for whatever reason.
+ * </p>
+ *
+ * <p>
+ * Say, for example, we're testing drivers on their ability to drive the robot.
+ * Of course, it would be incredibly moronic to force every single person to
+ * drive the robot the exact same way - how would that test skill, if the
+ * driver didn't feel comfortable driving the robot in the first place?
+ * </p>
+ *
+ * @author Colin Robertson
+ * @since 0.0.0
+ */
+public class IO implements ControlScheme {
+    /**
+     * The internally-used control scheme.
+     *
+     * <p>
+     * This can be changed on-the-fly to whatever you'd like. As Colin, I
+     * obviously prefer the Colin scheme - however, the driver of the robot
+     * should ultimately get the final say in terms of how the robot is
+     * controlled.
+     * </p>
+     *
+     * <p>
+     * In order to change this, you can simply instance a different control
+     * scheme. Examples:
+     * <ul>
+     *     <li>new Colin()</li>
+     *     <li>new Generic()</li>
+     * </ul>
+     * ... and whatever else you'd like.
+     * </p>
+     */
+    private final ControlScheme scheme = new Colin(0, 1, 2);
+
+    /**
+     * Desired swerve drive forwards power.
+     *
+     * <p>
+     * This is most often something like joystick Y.
+     * </p>
+     *
+     * @return desired forwards power.
+     */
+    @Override
+    public double getF() {
+        return scheme.getF();
+    }
+
+    /**
+     * Desired swerve drive strafe power.
+     *
+     * @return desired swerve drive strafe power.
+     */
+    @Override
+    public double getS() {
+        return scheme.getS();
+    }
+
+    /**
+     * Desired swerve drive turn power.
+     *
+     * @return desired swerve drive turn power.
+     */
+    @Override
+    public double getT() {
+        return scheme.getT();
+    }
+
+    /**
+     * Should the storage indexer be enabled?
+     *
+     * @return should the storage indexer be enabled or not.
+     */
+    @Override
+    public boolean getStorage() {
+        return scheme.getStorage();
+    }
+
+    /**
+     * Should the shooter be enabled?
+     *
+     * @return should the shooter be enabled?
+     */
+    @Override
+    public boolean getShooter() {
+        return scheme.getStorage();
+    }
+
+    /**
+     * Get the intake's intaking power.
+     *
+     * @return the intake's intaking power.
+     */
+    @Override
+    public double getIntake() {
+        return scheme.getIntake();
+    }
+
+    /**
+     * Get the intake's outtaking power.
+     *
+     * @return the intake's outtaking power.
+     */
+    @Override
+    public double getOuttake() {
+        return scheme.getOuttake();
+    }
+
+    /**
+     * Should the intake be going up?
+     *
+     * @return whether or not the intake should be going up.
+     */
+    @Override
+    public boolean getIntakeUp() {
+        return scheme.getIntakeUp();
+    }
+
+    /**
+     * Should the intake be going down?
+     *
+     * @return whether or not the intake should be going down.
+     */
+    @Override
+    public boolean getIntakeDown() {
+        return scheme.getIntakeDown();
+    }
+}
---
+++ src/main/java/frc/robot/controls/Colin.java
@@ -0,0 +1,144 @@
+package frc.robot.controls;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+
+/**
+ * Very simple control scheme designed for ease-of-use in development.
+ *
+ * <p>
+ * In other words, this is what I'd use if I were to drive the robot myself.
+ * It doesn't really matter if you like these controls or not - they're very
+ * easily hot-swappable. It would be pointless to require me to use a control
+ * scheme I'm not comfortable with, especially on somewhat of a time crunch.
+ * </p>
+ *
+ * @author Colin Robertson
+ * @since 0.0.0
+ */
+public class Colin implements ControlScheme {
+    /**
+     * Left stick.
+     */
+    private final Joystick stick1;
+
+    /**
+     * Right stick.
+     */
+    private final Joystick stick2;
+
+    /**
+     * Xbox controller.
+     */
+    private final XboxController controller;
+
+    /**
+     * Create a new generic control scheme provider.
+     *
+     * @param stick1     the left stick's port.
+     * @param stick2     the right stick's port.
+     * @param controller the controller's port.
+     */
+    public Colin(int stick1,
+                 int stick2,
+                 int controller) {
+        this.stick1 = new Joystick(stick1);
+        this.stick2 = new Joystick(stick2);
+        this.controller = new XboxController(controller);
+    }
+
+    /**
+     * Desired swerve drive forwards power.
+     *
+     * <p>
+     * This is most often something like joystick Y.
+     * </p>
+     *
+     * @return desired forwards power.
+     */
+    @Override
+    public double getF() {
+        return controller.getY(GenericHID.Hand.kRight);
+    }
+
+    /**
+     * Desired swerve drive strafe power.
+     *
+     * @return desired swerve drive strafe power.
+     */
+    @Override
+    public double getS() {
+        return controller.getX(GenericHID.Hand.kRight);
+    }
+
+    /**
+     * Desired swerve drive turn power.
+     *
+     * @return desired swerve drive turn power.
+     */
+    @Override
+    public double getT() {
+        return controller.getX(GenericHID.Hand.kLeft);
+    }
+
+    /**
+     * Should the storage indexer be enabled?
+     *
+     * @return should the storage indexer be enabled or not.
+     */
+    @Override
+    public boolean getStorage() {
+        return controller.getBumper(GenericHID.Hand.kLeft);
+    }
+
+    /**
+     * Should the shooter be enabled?
+     *
+     * @return should the shooter be enabled?
+     */
+    @Override
+    public boolean getShooter() {
+        return controller.getBumper(GenericHID.Hand.kRight);
+    }
+
+    /**
+     * Get the intake's intaking power.
+     *
+     * @return the intake's intaking power.
+     */
+    @Override
+    public double getIntake() {
+        return controller.getTriggerAxis(GenericHID.Hand.kRight);
+    }
+
+    /**
+     * Get the intake's outtaking power.
+     *
+     * @return the intake's outtaking power.
+     */
+    @Override
+    public double getOuttake() {
+        return controller.getTriggerAxis(GenericHID.Hand.kLeft);
+    }
+
+    /**
+     * Should the intake be going up?
+     *
+     * @return whether or not the intake should be going up.
+     */
+    @Override
+    public boolean getIntakeUp() {
+        return controller.getAButton();
+    }
+
+    /**
+     * Should the intake be going down?
+     *
+     * @return whether or not the intake should be going down.
+     */
+    @Override
+    public boolean getIntakeDown() {
+        return controller.getBButton();
+    }
+}
