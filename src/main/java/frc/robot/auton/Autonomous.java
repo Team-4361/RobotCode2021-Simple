5ff23304919d0aadbf102a1c9dbf3c62a5f89cef
Jordan Migneault <jmigneault21@students.roxbury.org>
2021-04-03 13:21:51 -0400

    Update the barrel and bounce paths. I did some testing using simulated robot translation and spline plotting, the barrel path should work.

--- src/main/java/frc/robot/auton/Autonomous.java
+++ src/main/java/frc/robot/auton/Autonomous.java
@@ -1,5 +1,6 @@
 package frc.robot.auton;
 
+import frc.robot.util.Arcs;
 import frc.robot.util.Curves;
 import me.wobblyyyy.edt.DynamicArray;
 import me.wobblyyyy.pathfinder.api.Pathfinder;
@@ -119,90 +120,57 @@ public class Autonomous {
      * @see me.wobblyyyy.pathfinder.trajectory.Spline
      * @see me.wobblyyyy.pathfinder.trajectory.SplineInterpolator
      */
-    public static DynamicArray<HeadingPoint> barrelPath = mutate(
-            /*
-             * We use an anonymous class initializer here to make the code
-             * more readable.
-             */
-            swap(new DynamicArray<>() {{
-                /*
-                 * Set up center points and radius measurements.
-                 *
-                 * These radius measurements are done in inches, as are the
-                 * points and everything else.
-                 */
+    public static final DynamicArray<HeadingPoint> barrelPath =
+                swap(new DynamicArray<>() {{
+                    final Point D5_CENTER = new Point(0, 150);
+                    final double D5_RADIUS = 24;
+                    final Point B8_CENTER = new Point(120, 240);
+                    final double B8_RADIUS = 24;
+                    final Point D10_CENTER = new Point(0, 300);
+                    final double D10_RADIUS = 24;
 
-                final Point D5_CENTER = new Point(0, 150);
-                final double D5_RADIUS = 24;
-                final Point B8_CENTER = new Point(120, 240);
-                final double B8_RADIUS = 24;
-                final Point D10_CENTER = new Point(0, 300);
-                final double D10_RADIUS = 24;
+                    DynamicArray<HeadingPoint> D5_A = Arcs.decreasingFrom(
+                            Arcs.INTERPOLATED_QUAD_1, D5_RADIUS, D5_CENTER);
+                    DynamicArray<HeadingPoint> D5_B = Arcs.decreasingFrom(
+                            Arcs.INTERPOLATED_QUAD_2, D5_RADIUS, D5_CENTER);
+                    DynamicArray<HeadingPoint> D5_C = Arcs.increasingFrom(
+                            Arcs.INTERPOLATED_QUAD_3, D5_RADIUS, D5_CENTER);
+                    DynamicArray<HeadingPoint> D5_D = Arcs.increasingFrom(
+                            Arcs.INTERPOLATED_QUAD_4, D5_RADIUS, D5_CENTER);
 
-                // FIXME
-                // i would (and we should) use the class
-                // me.wobblyyyy.pathfinder.geometry.CircleInterpolator
-                // instead of declaring a bunch of arrays based on interpolated
-                // arcs, but the project code here is too old to support
-                // newer versions of pathfinder. drive class would need to be
-                // refactored entirely (use RTransform instead of translations
-                // and rotations as it currently does) and some import locations
-                // related to complex geometry may have changed.
+                    DynamicArray<HeadingPoint> B8_A = Arcs.increasingFrom(
+                            Arcs.INTERPOLATED_QUAD_2, B8_RADIUS, B8_CENTER);
+                    DynamicArray<HeadingPoint> B8_B = Arcs.increasingFrom(
+                            Arcs.INTERPOLATED_QUAD_1, B8_RADIUS, B8_CENTER);
+                    DynamicArray<HeadingPoint> B8_C = Arcs.decreasingFrom(
+                            Arcs.INTERPOLATED_QUAD_4, B8_RADIUS, B8_CENTER);
 
-                DynamicArray<HeadingPoint> D5_A = Curves.increasingFrom(
-                        Curves.interpolatedQ2, D5_RADIUS, D5_CENTER);
-                DynamicArray<HeadingPoint> D5_B = Curves.increasingFrom(
-                        Curves.interpolatedQ1, D5_RADIUS, D5_CENTER);
-                DynamicArray<HeadingPoint> D5_C = Curves.decreasingFrom(
-                        Curves.interpolatedQ4, D5_RADIUS, D5_CENTER);
-                DynamicArray<HeadingPoint> D5_D = Curves.decreasingFrom(
-                        Curves.interpolatedQ3, D5_RADIUS, D5_CENTER);
+                    DynamicArray<HeadingPoint> D10_A = new DynamicArray<>(
+                            new HeadingPoint(0, 275, 0));
+                    DynamicArray<HeadingPoint> D10_B = Arcs.increasingFrom(
+                            Arcs.INTERPOLATED_QUAD_2, D10_RADIUS, D10_CENTER);
+                    DynamicArray<HeadingPoint> D10_C = Arcs.increasingFrom(
+                            Arcs.INTERPOLATED_QUAD_1, D10_RADIUS, D10_CENTER);
 
-                DynamicArray<HeadingPoint> B8_A = Curves.decreasingFrom(
-                        Curves.interpolatedQ2, B8_RADIUS, B8_CENTER);
-                DynamicArray<HeadingPoint> B8_B = Curves.decreasingFrom(
-                        Curves.interpolatedQ1, B8_RADIUS, B8_CENTER);
-                DynamicArray<HeadingPoint> B8_C = Curves.increasingFrom(
-                        Curves.interpolatedQ4, B8_RADIUS, B8_CENTER);
+                    DynamicArray<HeadingPoint> RETURN_TO_START = new DynamicArray<>(
+                            new HeadingPoint(24, 275, 0),
+                            new HeadingPoint(22, 150, 0),
+                            new HeadingPoint(20, 0, 0),
+                            new HeadingPoint(0, 0, 0)
+                    );
 
-                DynamicArray<HeadingPoint> D10_A = new DynamicArray<>(
-                        new HeadingPoint(0, 275, 0));
-                DynamicArray<HeadingPoint> D10_B = Curves.decreasingFrom(
-                        Curves.interpolatedQ1, D10_RADIUS, D10_CENTER);
-                DynamicArray<HeadingPoint> D10_C = Curves.decreasingFrom(
-                        Curves.interpolatedQ2, D10_RADIUS, D10_CENTER);
-
-                DynamicArray<HeadingPoint> RETURN_TO_START = new DynamicArray<>(
-                        new HeadingPoint(24, 275, 0),
-                        new HeadingPoint(22, 150, 0),
-                        new HeadingPoint(20, 0, 0)
-                );
-
-                /*
-                 * Add all of the points to the array, in order.
-                 *
-                 * D5 to B8 to D10 to the start zone.
-                 */
-
-                // once again, very bad and messy code,
-                // move to circle interpolator as soon as we have
-                // time to refactor the drive class
-
-                D5_A.itr().forEach(this::add);
-                D5_B.itr().forEach(this::add);
-                D5_C.itr().forEach(this::add);
-                D5_D.itr().forEach(this::add);
-                B8_A.itr().forEach(this::add);
-                B8_B.itr().forEach(this::add);
-                B8_C.itr().forEach(this::add);
-                D10_A.itr().forEach(this::add);
-                D10_B.itr().forEach(this::add);
-                D10_C.itr().forEach(this::add);
-                RETURN_TO_START.itr().forEach(this::add);
-            }}),   // end class initializer
-            false, // don't swap x
-            false  // don't swap y
-    ); // end field declaration
+                    D5_A.itr().forEach(this::add);
+                    D5_B.itr().forEach(this::add);
+                    D5_C.itr().forEach(this::add);
+                    D5_D.itr().forEach(this::add);
+                    B8_A.itr().forEach(this::add);
+                    B8_B.itr().forEach(this::add);
+                    B8_C.itr().forEach(this::add);
+                    D10_A.itr().forEach(this::add);
+                    D10_B.itr().forEach(this::add);
+                    D10_C.itr().forEach(this::add);
+                    RETURN_TO_START.itr().forEach(this::add);
+                }});
 
     // Bounce Path Autonomous Points
     // updated but not tested as of 4/1/2021
@@ -218,7 +186,8 @@ public class Autonomous {
         new HeadingPoint(60, 180, 0),
         new HeadingPoint(30, 180, 0),
         new HeadingPoint(-60, 180, 0),
-        new HeadingPoint(0, 240, 0)       
+        new HeadingPoint(10, 240, 0),
+        new HeadingPoint(0, 240, 0)
     ));
 
     public static DynamicArray<HeadingPoint> lightspeedCircuitPath = mutate(swap(new DynamicArray<>(
